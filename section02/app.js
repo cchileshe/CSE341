241f51f57b2c398a53069d5b6e44d7122de8426d
cchileshe <chi06001@byui.edu>
2021-09-20 14:29:35 +0200

    Revert "Working with Express js"
    This reverts commit 54c07a109b4c809511ee065a5a275731481455da.

--- section02/app.js
+++ section02/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const path = require('path');
 
 const express = require('express');
@@ -20,3 +21,24 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
+=======
+const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use('/users',(req, res, next) => {
+    console.log('in the middleware');
+    res.send('<h1>add product</h1>');
+    //next();
+});
+
+app.use('/', (req, res, next) => {
+    console.log('in the middleware');
+    res.send('<h1>default</h1>');
+});
+
+
+const server = http.createServer(app);
+
+server.listen(3000);
+>>>>>>> parent of 54c07a1 (Working with Express js)
