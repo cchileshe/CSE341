b5c877e56f59e1dfb410c3477867fddcabc71c0e
cchileshe <chi06001@byui.edu>
2021-09-28 10:45:32 +0200

    removed section02 folder

--- section02/teamActivity03/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productsController = require('../controllers/products.js');
-
-router.get('/', productsController.getProducts);
-router.get('/search', productsController.getSearchProducts);
-
-module.exports = router;
