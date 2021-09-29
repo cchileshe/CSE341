1a7b87e0fc9de5d9399caa321c5fbb0cd44ec7e9
cchileshe <chi06001@byui.edu>
2021-09-29 22:47:49 +0200

    back to working

--- app.js
+++ app.js
@@ -3,47 +3,25 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 
 const port = process.env.PORT || 3000;
 const User = require('./models/user');
 
-const MONGODB_URI =
-  'mongodb+srv://admin:admin@cse341.sm7ru.mongodb.net/shop';
-
 const app = express();
-const store = new MongoDBStore({
-  uri: MONGODB_URI,
-  collection: 'sessions'
-});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-  })
-});
-
-app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
+    User.findById('615429513404e8a5263845d5')
     .then(user => {
       req.user = user;
       next();
@@ -53,14 +31,15 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-.connect(MONGODB_URI)
-.then(result => {
-  User.findOne().then(user => {
+  .connect(
+    'mongodb+srv://admin:admin@cse341.sm7ru.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
+    User.findOne().then(user => {
       if (!user) {
         const user = new User({
           name: 'Tony Stark',
@@ -78,4 +57,6 @@ mongoose
     console.log(err);
   });
 
-
+// mongoConnect((client) => {
+//     app.listen(port);
+// });
